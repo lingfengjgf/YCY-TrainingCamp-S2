fc1401bc6b2ed773aad0bbee05ec9585a361b6c9
lingfeng <lingfengjgf@163.com>
2022-12-19 19:40:05 +0800

    场馆模型

--- src/day02.js
+++ src/day02.js
@@ -8,19 +8,19 @@ let mixer;
 // 创建场景
 const scene = new THREE.Scene();
 // 创建相机
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.01,10);
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.01,100);
 // 创建渲染器
 const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // 设置相机位置
-camera.position.set(0.3,0.3,0.5);
+camera.position.set(10,5,25);
 
 // 用户控制相机
 const controls = new OrbitControls(camera,renderer.domElement);
 
-scene.background = new THREE.Color(0.6,0.6,0.6);
+scene.background = new THREE.Color(0.2,0.2,0.2);
 
 // 环境光
 // const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
@@ -36,16 +36,48 @@ scene.add(directionLight);
 // const boxMesh = new THREE.Mesh(boxGeometry,boxMaterial);
 // scene.add(boxMesh);
 
-// 创建甜甜圈
-let donuts;
+// 创建场馆
 // 加载gltf/glb模型
-new GLTFLoader().load("../resources/models/donuts_new.glb",(gltf)=>{
+new GLTFLoader().load("../resources/models/test02.glb",(gltf)=>{
   // console.log(gltf);
   scene.add(gltf.scene);
-  donuts = gltf.scene;
-  // gltf.scene.traverse(child=>{
-  //   console.log(child.name);
-  // })
+
+  gltf.scene.traverse(child=>{
+    if(child.name=='2023'){
+      const video = document.createElement('video');
+      video.src = "../resources/yanhua.mp4";
+      video.muted = true;
+      video.autoplay = "autoplay";
+      video.loop = true;
+      video.play();
+      const videoTexture = new THREE.VideoTexture(video);
+      const videoMaterial = new THREE.MeshBasicMaterial({map: videoTexture});
+      child.material = videoMaterial;
+    }
+    if(child.name=='屏幕1'){
+      const video = document.createElement('video');
+      video.src = "../resources/video01.mp4";
+      video.muted = true;
+      video.autoplay = "autoplay";
+      video.loop = true;
+      video.play();
+      const videoTexture = new THREE.VideoTexture(video);
+      const videoMaterial = new THREE.MeshBasicMaterial({map: videoTexture});
+      child.material = videoMaterial;
+    }
+    if(child.name=='曲面屏'){
+      const video = document.createElement('video');
+      video.src = "../resources/video02.mp4";
+      video.muted = true;
+      video.autoplay = "autoplay";
+      video.loop = true;
+      video.play();
+      const videoTexture = new THREE.VideoTexture(video);
+      const videoMaterial = new THREE.MeshBasicMaterial({map: videoTexture});
+      child.material = videoMaterial;
+    }
+  })
+  // console.log('num2023:',num2023);
 
   mixer = new THREE.AnimationMixer(gltf.scene);
   const clips = gltf.animations;
@@ -62,7 +94,7 @@ new GLTFLoader().load("../resources/models/donuts_new.glb",(gltf)=>{
 
 // 加载环境光HDR图片
 new RGBELoader().load("../resources/sky.hdr",texture=>{
-  scene.background = texture;
+  // scene.background = texture;
   texture.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = texture;
   renderer.outputEncoding = THREE.sRGBEncoding;
@@ -77,9 +109,6 @@ function animate(){
 
   controls.update();
 
-  if(donuts){
-    donuts.rotation.y += 0.01;
-  }
 
   if(mixer){
     mixer.update(0.02);
